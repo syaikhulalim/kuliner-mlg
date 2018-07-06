9b6c1a33f3aa23c27047349fd31575a0a5b13f5a
M. Syaikhul Alim <syaikhulalim9@gmail.com>
2018-07-06 16:06:08 +0700

    update controller Home

--- CodeIgniter/application/controllers/Home.php
+++ CodeIgniter/application/controllers/Home.php
@@ -13,6 +13,11 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('home.php');
+		$idKuliner = $this->uri->segment(2);
+		$this->load->helper('url', 'form');
+		$data['kuliner_list'] = $this->db->where('
+			idKuliner', $idKuliner)->get('
+			kuliner_malang')->row_array();
+		$this->load->view('single_post', $data);
 	}
 }
