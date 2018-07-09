7076c6dddfd1d2dff757695ce895d40ae900b2b9
M. Syaikhul Alim <syaikhulalim9@gmail.com>
2018-07-09 08:18:54 +0700

    update view single_post

--- CodeIgniter/application/views/single_post.php
+++ CodeIgniter/application/views/single_post.php
@@ -81,7 +81,9 @@
     <!-- Main Content -->
     <div class="container">
         <div class="row">
-            <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
+            <div class="col-sm-2"></div>
+            <div class="col-sm-6">
+                <div class="jumbotron">
                 <div class="post-preview">
                     <h2 class="post-title">
                         <?=$kuliner_list['nama']?>
@@ -90,12 +92,26 @@
                         <?=$kuliner_list['alamat']?>
                     </p>
                     <p class="post-file">
-                        <img src="<?php echo base_url()?>assets/uploads/<?php echo $kuliner_list['foto']?>" alt="" width=100 height=100>
+                        <img src="<?php echo base_url()?>assets/uploads/<?php echo $kuliner_list['foto']?>" alt="" width=800 height=455>
                     <p class="text-justify">
                         <?=$kuliner_list['keterangan']?>
                     </p>
                 </div>
                 <hr>
+            </div>
+            </div>
+ 
+
+            <div class="col-sm-2">
+                <div class="jumbotron">
+                <form action="<?php echo base_url('index.php/item/hasil')?>" action="GET">
+                <div class="form-group">
+                    <label for="cari"><h2>Cari Data : </h2></label>
+                    <input type="text" class="form-control" id="cari" name="cari" placeholder="cari">
+                </div>
+                <input class="btn btn-primary" type="submit" value="Cari">
+                </form>
+                </div>
             </div>
         </div>
     </div>
